85d5de0a046a8fba948114d0e6f04fd0c1b127d5
DavidNNussbaum <brocknynj@aol.com>
2021-05-26 20:36:03 -0400

    doctors_controller and reviews_controller are preliminarily filled in.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,56 @@
 class DoctorsController < ApplicationController
+    before_action :set_doctor, only: [:show, :update, :destroy]
+
+    def index
+      @doctors = Doctor.all
+  
+      render json: @doctors
+    end
+  
+ 
+    def show
+      render json: @doctor
+    end
+  
+   
+    def create
+      @doctor = Doctor.new(doctor_params)
+  
+      if @doctor.save
+        render json: @doctor, status: :created, location: @doctor
+      else
+        render json: @doctor.errors, status: :unprocessable_entity
+      end
+    end
+  
+     
+    def update
+      if @doctor.update(doctor_params)
+        render json: @doctor
+      else
+        render json: @doctor.errors, status: :unprocessable_entity
+      end
+    end
+  
+    
+    def destroy
+      if @doctor.destroy
+        render json: {message: "Doctor successfully removed"}
+      else
+        render json: {message: "Something went wrong! Errors: #{@doctor.errors.full_messages}"}
+      end
+    end
+  
+    private
+       
+      def set_doctor
+        @doctor = Dotor.find(params[:id])
+      end
+  
+     
+      def doctor_params
+        params.require(:doctor).permit(:name, :address)
+      end
+  end 
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,58 @@
 class ReviewsController < ApplicationController
+    before_action :set_review, only: [:show, :update, :destroy]
+
+   
+  def index
+    @reviews = Review.all
+
+    render json: @reviews
+  end
+
+  
+  def show
+    render json: @review
+  end
+
+   
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      render json: @review, status: :created, location: @review
+    else
+      render json: @review.errors, status: :unprocessable_entity
+    end
+  end
+
+  
+  def update
+    if @review.update(review_params)
+      render json: @review
+    else
+      render json: @review.errors, status: :unprocessable_entity
+    end
+  end
+
+   
+  def destroy
+    if @review.destroy
+      render json: {message: "Review successfully removed"}
+    else
+      render json: {message: "Something went wrong! Errors: #{@review.errors.full_messages}"}
+    end
+  end
+
+  private
+     
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+     
+    def review_params
+      params.require(:review).permit(:doctor_rating, :doctor_comments, :doctor_office_rating, :doctor_office_comments, :doctor_id, :user_id )
+    end
 end
+end
+
+ 
