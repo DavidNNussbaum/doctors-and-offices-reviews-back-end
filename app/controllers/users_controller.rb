f9f1838a862a6ec5ba7a27434c9e791d6ed03df6
DavidNNussbaum <brocknynj@aol.com>
2021-06-06 09:32:01 -0400

    User signin is modified.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
      
   before_action :find_user, only: [:show, :edit, :update, :destroy]
    
-
-   
   def show
     render json: UserSerializer.new(@user).serializable_hash 
   end
@@ -13,14 +11,14 @@ class UsersController < ApplicationController
 
     render json: UserSerializer.new(@users).serializable_hash 
   end
-
-
    
   def create
-    @user = User.new(user_params)
-
+    @user = User.find_by(email: user_params[:email])
+    if @user.nil?
+      @user = User.new(user_params)
+    end
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: UserSerializer.new(@user).serializable_hash , status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
